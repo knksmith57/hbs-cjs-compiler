be76f420c9f75aa2e344ee396e89f96e206d0d0d
Kyle Smith <kbsmith@rmn.com>
2016-06-11 23:12:05 -0500

    switch to js standard style. fixes #5

--- bin/hbs-cjs-compiler.js
+++ bin/hbs-cjs-compiler.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const fs    = require('fs');
-const yargs = require('yargs');
+const fs = require('fs')
+const yargs = require('yargs')
 
-var argv, cli, helpers, namespace, templateDir;
+var argv, cli, helpers, namespace, templateDir
 
 cli = yargs
   .usage(
-`Compiles handlebars templates into CommonJS modules.
+    `Compiles handlebars templates into CommonJS modules.
+
 Usage: $0 [options] <directory>`)
   .example('$0 templates', 'Compile "templates" directory and print to stdout')
   .example('$0 --known join --known modChoose --file myTemplates.js templates',
@@ -16,7 +17,7 @@ Usage: $0 [options] <directory>`)
   .example('$0 -k join -f myTemplates.js -n bam templates',
     'Compile "templates" directory, output to myTemplates.js, hint about join helper, namespace templates with bam/')
   .version(() => {
-    return require('../package.json').version;
+    return require('../package.json').version
   })
   .alias('f', 'file')
   .describe('f', 'Output file')
@@ -25,35 +26,35 @@ Usage: $0 [options] <directory>`)
   .alias('k', 'known')
   .describe('k', 'Known helper')
   .alias('n', 'namespace')
-  .describe('n', 'Template namespace (prefix)');
+  .describe('n', 'Template namespace (prefix)')
 
-argv = cli.argv;
-templateDir = argv._;
-helpers = [];
-namespace = argv.namespace || '';
+argv = cli.argv
+templateDir = argv._
+helpers = []
+namespace = argv.namespace || ''
 
 if (argv.known) {
-  helpers = argv.known instanceof Array ? argv.known : [argv.known];
+  helpers = argv.known instanceof Array ? argv.known : [argv.known]
 }
 
 // must specify exactly one template directory
-if (1 !== templateDir.length) {
-  console.error('Exactly 1 template directory must be provided\n');
-  console.error(cli.help());
-  process.exit(1);
+if (templateDir.length !== 1) {
+  console.error('Exactly 1 template directory must be provided\n')
+  console.error(cli.help())
+  process.exit(1)
 }
 
-templateDir = templateDir[0];
+templateDir = templateDir[0]
 
 require('..').asScript(templateDir, {helpers: helpers, namespace: namespace})
   .catch((err) => {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   })
   .then((script) => {
     if (argv.file) {
-      fs.writeFileSync(argv.file, script);
+      fs.writeFileSync(argv.file, script)
     } else {
-      process.stdout.write(script);
+      process.stdout.write(script)
     }
-  });
+  })
--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-'use strict';
+'use strict'
 
-const async        = require('async');
-const debug        = require('debug')('hbs-cjs-precompiler');
-const fs           = require('fs');
-const glob         = require('glob');
-const handlebars   = require('handlebars');
-const minify       = require('uglify-js').minify;
-const path         = require('path');
-const procs        = require('os').cpus().length;
-const resolveBin   = require('resolve-bin');
-const spawn        = require('child_process').spawn;
-const sprintf      = require('util').format;
-const strToModule  = require('require-from-string');
+const async = require('async')
+const debug = require('debug')('hbs-cjs-precompiler')
+const fs = require('fs')
+const glob = require('glob')
+const handlebars = require('handlebars')
+const minify = require('uglify-js').minify
+const path = require('path')
+const procs = require('os').cpus().length
+const resolveBin = require('resolve-bin')
+const spawn = require('child_process').spawn
+const sprintf = require('util').format
+const strToModule = require('require-from-string')
 
-const spawnCommand = resolveBin.sync('handlebars');
+const spawnCommand = resolveBin.sync('handlebars')
 
 /** @type {asScript} */
-module.exports.asScript = asScript;
+module.exports.asScript = asScript
 
 /** @type {asModule} */
-module.exports.asModule = asModule;
-
+module.exports.asModule = asModule
 
 const moduleTemplateFn = handlebars.compile(
   fs.readFileSync(path.resolve(__dirname, 'moduleTemplate.hbs')).toString()
-);
-
+)
 
 /**
  * Compiles a directory of handlebars templates and returns a CommonJS
@@ -36,16 +34,15 @@ const moduleTemplateFn = handlebars.compile(
  *
  * @returns {Promise}
  */
-function asModule(templateDir, options) {
+function asModule (templateDir, options) {
   return new Promise((resolve, reject) => {
     asScript(templateDir, options)
       .catch(reject)
       .then((moduleStr) => {
-        resolve(strToModule(moduleStr));
-      });
-  });
+        resolve(strToModule(moduleStr))
+      })
+  })
 }
-
 
 /**
  * Compiles a directory of handlebars templates and generates a CommonJS
@@ -56,44 +53,45 @@ function asModule(templateDir, options) {
  *
  * @returns {Promise}
  */
-function asScript(templateDir, options) {
-  var fileGlob, helpers, namespace, terminating = false, workerFns = [], workers = {};
+function asScript (templateDir, options) {
+  var terminating = false
+  var workerFns = []
+  var workers = {}
 
-  helpers = options.helpers || [];
-  namespace = options.namespace ? options.namespace + '/' : '';
+  var helpers = options.helpers || []
+  var namespace = options.namespace ? options.namespace + '/' : ''
 
   // handle killing child processes gracefully
-  process.on('SIGTERM', onTerminate);
-  process.on('SIGINT', onTerminate);
+  process.on('SIGTERM', onTerminate)
+  process.on('SIGINT', onTerminate)
 
-  templateDir = path.resolve(process.cwd(), templateDir);
-  fileGlob = path.resolve(templateDir, '**/*.hbs');
+  templateDir = path.resolve(process.cwd(), templateDir)
+  var fileGlob = path.resolve(templateDir, '**/*.hbs')
 
   return new Promise((resolve, reject) => {
     glob(fileGlob, {baseDir: templateDir, follow: true}, (err, templates) => {
       if (err) {
-        debug('error globbing for templates');
-        return reject(new Error(err));
+        debug('error globbing for templates')
+        return reject(new Error(err))
       }
 
-      debug('found %d templates: %s', templates.length, JSON.stringify(templates, null, 2));
+      debug('found %d templates: %s', templates.length, JSON.stringify(templates, null, 2))
 
       workerFns = templates.map((template) => {
-        return createWorker.bind(null, template);
-      });
+        return createWorker.bind(null, template)
+      })
 
-      debug('compiling %d templates using a max of %d processes', templates.length, procs);
+      debug('compiling %d templates using a max of %d processes', templates.length, procs)
 
       async.parallelLimit(workerFns, procs, (workerErr, results) => {
         if (workerErr) {
-          return reject(new Error(workerErr));
+          return reject(new Error(workerErr))
         }
 
-        resolve(moduleTemplateFn({templateFns: results}));
-      });
-    });
-  });
-
+        resolve(moduleTemplateFn({templateFns: results}))
+      })
+    })
+  })
 
   /**
    * Spawns a worker to compile handlebars template specified by `partial`.
@@ -103,29 +101,31 @@ function asScript(templateDir, options) {
    *
    * @returns {void}
    */
-  function createWorker(partial, cb) {
-    var bufs = [], handlebarsArgs = [], output, partialName, worker;
+  function createWorker (partial, cb) {
+    var bufs = []
+    var handlebarsArgs = []
+    var output
 
     if (terminating) {
-      return cb(new Error('process terminating'));
+      return cb(new Error('process terminating'))
     }
 
-    partialName = partialPathToName(partial);
+    var partialName = partialPathToName(partial)
 
-    debug('precompiling %s', partialName);
+    debug('precompiling %s', partialName)
 
-    handlebarsArgs.push('--simple');
+    handlebarsArgs.push('--simple')
 
     // add arguments for all specified-known helpers
     helpers.forEach((helper) => {
-      handlebarsArgs.push('--known', helper);
-    });
+      handlebarsArgs.push('--known', helper)
+    })
 
-    worker = spawn(spawnCommand, handlebarsArgs.concat(partial));
+    var worker = spawn(spawnCommand, handlebarsArgs.concat(partial))
 
-    workers[worker.pid] = worker;
+    workers[worker.pid] = worker
 
-    worker.stdout.on('data', bufs.push.bind(bufs));
+    worker.stdout.on('data', bufs.push.bind(bufs))
     worker.stdout.on('end', () => {
       output = {
         fn: minify(sprintf(
@@ -133,21 +133,20 @@ function asScript(templateDir, options) {
           Buffer.concat(bufs).toString()
         ), {fromString: true}).code,
         name: partialName
-      };
-    });
-    worker.stderr.pipe(process.stderr);
+      }
+    })
+    worker.stderr.pipe(process.stderr)
 
     worker.on('close', (code) => {
-      delete workers[worker.pid];
+      delete workers[worker.pid]
 
-      if (0 !== code) {
-        return cb(new Error('failed to precompile ' + partialName));
+      if (code !== 0) {
+        return cb(new Error('failed to precompile ' + partialName))
       }
 
-      cb(null, output);
-    });
+      cb(null, output)
+    })
   }
-
 
   /**
    * Generates partial name from the specified partial path.
@@ -155,30 +154,29 @@ function asScript(templateDir, options) {
    * @param {String} partial path to partial
    * @returns {String}
    */
-  function partialPathToName(partial) {
+  function partialPathToName (partial) {
     // via http://stackoverflow.com/a/4250408
-    return namespace + path.relative(templateDir, String(partial)).replace(/\.[^/.]+$/, '');
+    return namespace + path.relative(templateDir, String(partial)).replace(/\.[^/.]+$/, '')
   }
-
 
   /**
    * Terminates all active workers.
    * @returns {void}
    */
-  function onTerminate() {
+  function onTerminate () {
     if (terminating) {
-      return;
+      return
     }
 
-    terminating = true;
+    terminating = true
 
     // kill all workers
-    var workerPids = Object.keys(workers);
+    var workerPids = Object.keys(workers)
     if (workerPids) {
       workerPids.forEach((pid) => {
-        console.error('sending kill signal to worker %s', pid);
-        workers[pid].kill();
-      });
+        console.error('sending kill signal to worker %s', pid)
+        workers[pid].kill()
+      })
     }
   }
 }
